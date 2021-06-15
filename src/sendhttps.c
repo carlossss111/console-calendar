edf94e68b264271dd1747f17f3adac9c28068201
LinuxDaniel <danielrobinson1112@gmail.com>
2021-06-15 17:35:31 +0100

    Added custom URL and Header parameters
    Successfully gets 401 reply.
    No memleaks/errs

--- src/sendhttps.c
+++ src/sendhttps.c
@@ -37,27 +37,34 @@ static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, voi
 /* Send HTTP request
 * (value returned requires freeing)
 */
-char* httpsGET()
+char* httpsGET(char* myURL, int numOfMyHeaders, char* myHeaders[])
 {
 	CURL *curl;
 	CURLcode res;
-
+	struct curl_slist *headers = NULL;
 	struct MemoryStruct response;
+	int i;
+
 	response.memory = malloc(1);
 	response.size = 0;
 
 	curl_global_init(CURL_GLOBAL_ALL);
 	curl = curl_easy_init();
 
+	/*apply headers*/
+	for(i = 0; i < numOfMyHeaders; i++){
+		headers = curl_slist_append(headers, myHeaders[i]);
+	}
+	if(numOfMyHeaders)
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+	curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
 	/*specify URL*/
-	curl_easy_setopt(curl, CURLOPT_URL, "https://www.example.com/");
+	curl_easy_setopt(curl, CURLOPT_URL, myURL);
 
 	#ifdef DEBUG
         curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
     #endif
-
-	/*user agent header*/
-	curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
 
 	/*write instructions to memory*/
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
@@ -68,12 +75,14 @@ char* httpsGET()
 
 	/*error check*/
 	if(res != CURLE_OK) {
-    	fprintf(stderr, "err ln%d: %s\n",__LINE__,curl_easy_strerror(res));
+		fprintf(stderr, "err ln%d: %s\n",__LINE__,curl_easy_strerror(res));
 		exit(EXIT_FAILURE);
 	}
 
 	/*frees*/
 	curl_easy_cleanup(curl);
+	if(numOfMyHeaders)
+		curl_slist_free_all(headers);
 	curl_global_cleanup();
 
 	return response.memory;
@@ -84,7 +93,12 @@ char* httpsGET()
 */
 int main(int argc, char** argv){
 	char* myResponse;
-	myResponse = httpsGET();
+	char* myHeaders[1];
+	char* myURL = "https://graph.microsoft.com/v1.0/me/";
+
+	myHeaders[0] = "Host: graph.microsoft.com";
+	
+	myResponse = httpsGET(myURL, 1, myHeaders);
 	printf("%s\n",myResponse);
 	free(myResponse);
 	return 0;

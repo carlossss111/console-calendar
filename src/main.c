dee754eca5324923571493cce2e48392c9d91cde
Daniel <danielrobinson1112@gmail.com>
2021-08-16 10:57:59 +0100

    prints all calendar event subjects

--- src/main.c
+++ src/main.c
@@ -6,41 +6,45 @@
 #include "sendhttps.h"
 #include "jsmn.h"
 
+/*networking*/
 #define NUM_OF_HEADERS 3
 
+/*JSON parsing*/
+#define SIZE_OF_CALENDAR_EVENT 115
+
 /*structure for grouping JSON properties*/
-typedef struct JsonResponse{
+typedef struct JsonWrapper{
 		char *raw;
 		int tokenTotal;
 		jsmntok_t *tokenPtr; /*nested struct*/
 		jsmn_parser parser;  /*nested struct*/
-}JsonResponse;
+}JsonWrapper;
 
 /*wrapper for jsmn_parse cause why not*/
-JsonResponse jsmnParseWrapper(JsonResponse json){
-	jsmn_init(&json.parser);
+int jsmnParseWrapper(JsonWrapper *json){
+	jsmn_init(&json->parser);
 
 	/*load number of available tokens into json.tokenTotal*/
-	if(json.tokenTotal < 0){
-		if((json.tokenTotal = jsmn_parse(&json.parser, json.raw, strlen(json.raw), NULL, 0)) < 0){
-			fprintf(stderr, "err ln%d: Error code (%d) when parsing JSON UNKNOWN size.\n", __LINE__, json.tokenTotal);
+	if(json->tokenTotal < 0){
+		if((json->tokenTotal = jsmn_parse(&json->parser, json->raw, strlen(json->raw), NULL, 0)) < 0){
+			fprintf(stderr, "err ln%d: Error code (%d) when parsing JSON UNKNOWN size.\n", __LINE__, json->tokenTotal);
 			exit(EXIT_FAILURE);
 		}
 	}
 	/*if json.tokenTotal is known, parse it properly and load the tokens into json.tokenPtr*/
 	else{
-		if((jsmn_parse(&json.parser, json.raw, strlen(json.raw), json.tokenPtr, json.tokenTotal)) < 0){
-			fprintf(stderr, "err ln%d: Error code (%d) when parsing JSON of KNOWN size.\n", __LINE__, json.tokenTotal);
+		if((jsmn_parse(&json->parser, json->raw, strlen(json->raw), json->tokenPtr, json->tokenTotal)) < 0){
+			fprintf(stderr, "err ln%d: Error code (%d) when parsing JSON of KNOWN size.\n", __LINE__, json->tokenTotal);
 			exit(EXIT_FAILURE);
 		}
 	}
 
-	return json;
+	return 0;
 }
 
 /*return whether a given json token is equal to a string*/
-static int jsoneq(const char *entireJson, jsmntok_t token, const char *comparisonString) {
-  if (token.type == JSMN_STRING && (int)strlen(comparisonString) == token.end - token.start &&
+int jsoneq(const char *entireJson, jsmntok_t token, const char *comparisonString) {
+  if (token.type == JSMN_STRING && (int)strlen(comparisonString) == token.end - token.start && \
       strncmp(entireJson + token.start, comparisonString, token.end - token.start) == 0) {
     return 1;
   }
@@ -48,34 +52,39 @@ static int jsoneq(const char *entireJson, jsmntok_t token, const char *compariso
 }
 
 /*return number of calendar events from a given JSON*/
-int numOfEvents(char *jsonString, jsmntok_t *tokenArr, int totalTokens){
+/*returns -1 if not found*/
+int eventCount(JsonWrapper json){
 	int i = 0;
 
 	/*find string matching "value" and stop*/
-	while(i < totalTokens && !jsoneq(jsonString, tokenArr[i], "value"))
+	while(i < json.tokenTotal && !jsoneq(json.raw, json.tokenPtr[i], "value"))
 		i++;
 
 	/*the next token (after "value") should be an array of calendar events*/
-	if(i != totalTokens && tokenArr[++i].type == JSMN_ARRAY)
-		return tokenArr[i].size;
+	if(i != json.tokenTotal && json.tokenPtr[++i].type == JSMN_ARRAY)
+		return json.tokenPtr[i].size;
 	return -1;
 }
 
-/*find the search value*/
-/*returns -1 if not found*/
-int findJsonValue(char *searchString, char *jsonString, jsmntok_t *tokenArr, int totalTokens){
-	int i = 0;
+/*given a key and an object token, print the corresponding value if in the object token*/
+int printKeyValue(JsonWrapper json, char *searchKey, int objToken){
+	int i = objToken;
+	int objEnd = json.tokenPtr[objToken].end;
 
-	/*find key matching "value" and stop*/
-	while(i < totalTokens && !jsoneq(jsonString, tokenArr[i], searchString))
+	/*check an object type has been provided*/
+	if(json.tokenPtr[objToken].type != JSMN_OBJECT)
+		return 1;
+
+	/*navigate to key*/
+	while(json.tokenPtr[i].start < objEnd && !jsoneq(json.raw, json.tokenPtr[i], searchKey))
 		i++;
+	if(json.tokenPtr[i].start >= json.tokenPtr[objToken].end)
+		return 1;
 
-	/*check a value is actually found*/
-	if(i == totalTokens)
-		return -1;
-
-	/*return corresponding value*/
-	return i;
+	/*print value corresponding to key*/
+	i++;
+	printf("%.*s",json.tokenPtr[i].end - json.tokenPtr[i].start,json.raw + json.tokenPtr[i].start);
+	return 0;
 }
 
 /*
@@ -86,11 +95,15 @@ int main(int argc, char** argv){
 	char *myResponse, *myHeaders[NUM_OF_HEADERS], *authkey;
 	char *myURL = "https://graph.microsoft.com/v1.0/me/calendarview/?startdatetime=2021-04-13T00:59:59.000Z&enddatetime=2021-04-13T23:59:59.999Z";
 
+	int i;
+	int j;
+
 	/*for json parsing*/
-	JsonResponse json;
-	json.raw = NULL;
-	json.tokenTotal = -1;
-	json.tokenPtr = NULL;
+	JsonWrapper *json;
+	json = malloc(sizeof(JsonWrapper));
+	json->raw = NULL;
+	json->tokenTotal = -1;
+	json->tokenPtr = NULL;
 
 	/*get key from file*/
 	authkey = readKey("authkey.txt");
@@ -103,7 +116,9 @@ int main(int argc, char** argv){
 
 	/*send HTTP GET request and print response*/
 	myResponse = httpsGET(myURL, NUM_OF_HEADERS, myHeaders);
+	#ifdef temp
 	printf("%s\n",myResponse);
+	#endif
 
 	/*check auth key*/
 	if(strstr(myResponse, "InvalidAuthenticationToken") != NULL) {
@@ -112,21 +127,42 @@ int main(int argc, char** argv){
 	}
 
 	/*get number of tokens for json.tokenTotal*/
-	json.raw = myResponse;
-	json = jsmnParseWrapper(json);
+	json->raw = myResponse;
+	jsmnParseWrapper(json);
 
 	/*allocate memory for the json.tokenPtr and populate it*/
-	json.tokenPtr = malloc(json.tokenTotal * sizeof(jsmntok_t));
-	json = jsmnParseWrapper(json); /*the json.tokenPtr is populated here*/
+	json->tokenPtr = malloc(json->tokenTotal * sizeof(jsmntok_t));
+	jsmnParseWrapper(json); /*the json.tokenPtr is populated here*/
 
 	/*print number of events*/
-	printf("Number of events on given day: %d\n", numOfEvents(json.raw, json.tokenPtr, json.tokenTotal));
+	printf("Number of events on given day: %d\n", eventCount(*json));
+
+	/*navigate j to list of calendar events*/
+	j = 0;
+	while(j < json->tokenTotal && !jsoneq(json->raw, json->tokenPtr[j], "value"))
+		j++;
+	if(j >= json->tokenTotal || json->tokenPtr[++j].type != JSMN_ARRAY)
+		return 1;
+	if(j >= json->tokenTotal || json->tokenPtr[++j].type != JSMN_OBJECT)
+		return 1;
+	
+	/*loop through each event*/
+	for(i = 0; i < eventCount(*json);i++){
+
+		/*print calendar event*/
+		printKeyValue(*json, "subject", j);
+		putchar('\n');
+
+		/*go to next calendar event*/
+		j += SIZE_OF_CALENDAR_EVENT; /*probably should replace with something more adaptable*/
+	}
 
 	/*frees*/
-	free(myResponse);/*shared by json.raw*/
 	free(myHeaders[1]);
 	free(authkey);
-	free(json.tokenPtr);
+	free(json->raw);
+	free(json->tokenPtr);
+	free(json);
 
 	return 0;
 }
